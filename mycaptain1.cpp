bf199d0aee71ee6fca69734da17e19e70f9f2945
prernabansal984 <69748920+prernabansal984@users.noreply.github.com>
2021-05-29 00:33:02 +0530

    created by PRERNA BANSAL.

---
+++ mycaptain1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of char : "<<sizeof(char) <<"byte \n";
+    cout<<"size of int : "<<sizeof(int) <<"byte \n";
+    cout<<"size of float : "<<sizeof(float) <<"byte \n";
+    cout<<"size of double : "<<sizeof(double) <<"byte \n";
+}
